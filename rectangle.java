6bd4bc3631ec8e94bfb65a08b5c1426b5bafceea
ryanC06 <350103776@gapps.yrdsb.ca>
2023-04-21 08:38:44 -0400

    updated code to the rectangle code

--- rectangle.java
+++ rectangle.java
@@ -1,6 +1,36 @@
-public class Rectangle(){
-  int height;
-  int length;
-  
-  Rectangle x = new Rectangle ( height , length);
+public class Rectangle
+{
+    private int width;
+    private int height;
+    
+    public Rectangle(int rectWidth, int rectHeight)
+    {
+        width = rectWidth;
+        height = rectHeight;
+    }
+    
+    public int getArea()
+    {
+        return width * height;
+    }
+    
+    public int getHeight()
+    {
+        return height;
+    }
+    
+    public int getWidth()
+    {
+        return width;
+    }
+    
+    public String toString()
+    {
+        return "Rectangle with width: " + width + " and height: " + height;
+    }
+    
+    public int getPerimeter(){
+        return 2 * height + 2*width;
+    }
+    
 }
